3c2bff117276c7feffc51ded09ceb19e33741720
Soen SOUPRAYEN <soen.souprayen@edu.esiee.fr>
2026-02-13 00:04:51 +0000

    ReadME

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# DataEngineering
-Projet DataEngineering
+# Documentation du Crawler ParuVendu
+
+Ce projet consiste à recupérer des données sur un site  en utilisant **Scrapy**. On procède donc à une récuperation automatique des annonces automobiles sur le site ParuVendu, de nettoyer les données, et de les stocker dans une base de données **MongoDB** et ensuite de les afficher en utilisant **Streamlit**.
+
+##  Structure des fichiers de Scraping
+
+Voici le rôle détaillé de chaque fichier présent dans le dossier `scrappy/crawler/`.
+
+### 1. `paruvendu_spider.py`
+C'est le cœur du projet. Ce script est responsable de la navigation sur le site web.
+
+* **Rôle :** Il envoie les requêtes HTTP au site ParuVendu. Il récupère le HTML brut (Titre, Prix, Description, Lien).
+* **Pagination :** Il détecte automatiquement le lien "Page suivante" pour parcourir toutes les pages de résultats.
+* **Configuration :** Il définit le `USER_AGENT` pour imiter un navigateur et éviter d'être bloqué.
+
+### 2. `pipelines.py` 
+C'est ici que la donnée brute est transformée en donnée propre et exploitable. Une fois que le spider a trouvé une annonce, il l'envoie ici.
+
+* **Filtrage :** Il rejette les annonces indésirables grâce à une liste (`EXCLUDED_KEYWORDS`) : tracteurs, jantes, pneus, locations, etc.
+
+* **Extraction Intelligente :**
+    * **Rôle :** Il utilise regex pour determiner les informations suivantes :
+        * **Année** 
+        * **Kilométrage :** (Nettoie et convertit le kilométrage en nombre entier.)
+        * **Prix** 
+* **Normalisation :** Donne et index les marques (ex: "Vw" devient "Volkswagen") et détecte le type de boîte de vitesse et l'énergie.
+* **Stockage :** Se connecte à **MongoDB** et sauvegarde l'annonce.
+
+### 3. `database.py`
+Ce fichier sert de point central pour la configuration de la base de données.
+
+### 4. `docker-compose.yml`
+Ce fichier permet de lancer tout l'environnement en une seule commande.
+
+* **Rôle :** Il crée et gère deux codes ci-dessus qui fonctionnent ensemble :
+    1.  **MongoDB :** La base de données qui tourne dans un conteneur .
+    2.  **Crawler :** Le code Python qui s'exécute.
+
+Il relie les deux  pour qu'ils puissent communiquer.
+
+
+## Comment lancer le projet
+
+1.  Assurez-vous d'avoir **Docker** installé.
+2.  Placez-vous dans le dossier racine du projet.
+3.  Lancez la commande :
+
+```bash
+docker-compose up --build
--- scrappy/crawler/pipelines.py
+++ scrappy/crawler/pipelines.py
@@ -11,8 +11,7 @@ class MongoPipeline:
         self.BRAND_MAPPING = {
             "Ds": "Citroën", "DS": "Citroën", "Vw": "Volkswagen", "Land Rover": "Land Rover"
         }
-        
-        # LISTE NOIRE (Pour virer les jantes et chaussettes)
+
         self.EXCLUDED_KEYWORDS = [
             "TRACTEUR", "CAMION", "REMORQUE", "CARAVANE", "MOTO", "SCOOTER", "QUAD", "BUGGY", "VAN",
             "JANTE", "PNEU", "ROUE", "CASQUE", "CHAINE", "CHAUSSETTE", "PIECE", "MOTEUR", 
@@ -25,24 +24,19 @@ class MongoPipeline:
         titre = item.get('titre', '')
         lien = item.get('lien', '')
         
-        # --- 1. FILTRE ANTI-POUBELLE ---
+
         titre_upper = titre.upper()
         for keyword in self.EXCLUDED_KEYWORDS:
             if keyword in titre_upper:
                 raise DropItem(f"❌ Ignoré : {titre}")
 
-        # --- 2. ANNÉE ---
         annee = None
         annee_match = re.search(r'(199\d|20[0-3]\d)', raw_text)
         if annee_match:
             annee = int(annee_match.group(1))
 
-        # --- 3. KILOMÉTRAGE (CORRECTION ICI) ---
+  
         km = None
-        # Explication du Regex magique :
-        # (?:^|[\D]) -> Doit commencer soit au début, soit après un caractère qui N'EST PAS un chiffre (l'espace).
-        # (\d{1,3}(?:\s?\d{3})?) -> Capture 1 à 3 chiffres, suivis optionnellement d'un espace et 3 chiffres.
-        # Cela force un maximum de 6 chiffres (999 999 km), donc impossible de coller l'année !
         km_match = re.search(r'(?:^|[\D])(\d{1,3}(?:\s?\d{3})?)\s*km', raw_text, re.IGNORECASE)
         
         if km_match:
@@ -50,7 +44,6 @@ class MongoPipeline:
             if clean_km.isdigit():
                 km = int(clean_km)
 
-        # --- 4. MARQUE ---
         marque = "Inconnue"
         url_match = re.search(r'voiture-occasion/([^/]+)/', lien)
         if url_match:
@@ -58,8 +51,6 @@ class MongoPipeline:
         else:
             marque_brute = titre.split(' ')[0].strip().capitalize() if titre else "Inconnue"
         marque = self.BRAND_MAPPING.get(marque_brute, marque_brute)
-
-        # --- 5. ENERGIE & BOITE ---
         boite = "Manuelle"
         if "auto" in raw_text.lower() or "bva" in raw_text.lower(): boite = "Automatique"
             
@@ -71,8 +62,6 @@ class MongoPipeline:
         elif "electrique" in text_low or "électrique" in text_low: energie = "Electrique"
 
         prix_clean = "".join(re.findall(r'\d+', item.get('prix_brut', '')))
-        
-        # --- 6. SAUVEGARDE ---
         document = {
             'titre': titre,
             'marque': marque,
